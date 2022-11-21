2dc5498fb19f5ffb339ade671edcadb1cb6b8f66
Nicky <nickysauceofficial@gmail.com>
2022-11-21 22:45:37 +0530

    recursive string manipulation q:1, remove letter from word recursively added

--- Recursion/String-Manipulation.js
+++ Recursion/String-Manipulation.js
@@ -1,3 +1,13 @@
-console.log("String Manipulation");
+// Q: remove a letter from word recursively
 
-const str = "amazon rain forest";
+console.log("String Manipulation");
+const string = "baccadealaoaoommma";
+
+function removeA(string) {
+  if (string < 2) return "";
+  return string.slice(0, 1) === "a"
+    ? removeA(string.slice(1))
+    : string.slice(0, 1) + removeA(string.slice(1));
+}
+
+console.log(removeA(string));
