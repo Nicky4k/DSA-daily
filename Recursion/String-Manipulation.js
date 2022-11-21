9a300dfa807413d73d1fd650af9b285718fd4b37
Nicky <nickysauceofficial@gmail.com>
2022-11-21 23:47:30 +0530

    solve removing words using starts with, recursively

--- Recursion/String-Manipulation.js
+++ Recursion/String-Manipulation.js
@@ -11,3 +11,8 @@ function removeA(string) {
 }
 
 console.log(removeA(string));
+
+const poem =
+  "the apple fell from the apple tree, did you see the apple falling down the apple tree? Watching an apple fall from apple tree is amazing, as we all love apples, don't we love apple and apple trees.";
+console.log(poem.split(" "));
+console.log(poem.startsWith("apple"));
