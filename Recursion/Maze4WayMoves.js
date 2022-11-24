3ee9a2f0958e67d02bffdf8ed4b21b4fac7d749c
Nicky <nickysauceofficial@gmail.com>
2022-11-24 18:26:15 +0530

    backtracking done

--- Recursion/Maze4WayMoves.js
+++ Recursion/Maze4WayMoves.js
@@ -10,22 +10,35 @@ function diagonalMaze(a, b, board, path) {
   let left = 0;
   let right = 0;
   let diag = 0;
+  let up = 0;
+  let down = 0;
 
+  // Backtracking
+  board[a][b] = false;
+
+  if (a > 0) {
+    up = diagonalMaze(a - 1, b, board, path + "Up ");
+  }
   if (a < board.length - 1) {
-    left = diagonalMaze(a + 1, b, board, path + "Down ");
+    down = diagonalMaze(a + 1, b, board, path + "Down ");
   }
   if (b < board[0].length - 1) {
     right = diagonalMaze(a, b + 1, board, path + "Right ");
+  }
+  if (b > 0) {
+    left = diagonalMaze(a, b - 1, board, path + "Left ");
   }
   if (a < board.length - 1 && b < board[0].length - 1) {
     diag = diagonalMaze(a + 1, b + 1, board, path + "Diagonal ");
   }
 
-  return left + right + diag;
+  // Backtracking
+  board[a][b] = true;
+
+  return left + right + up + down + diag;
 }
 const board = [
   [true, true, true],
-  [false, false, true],
-  [false, true, true],
+  [true, true, true],
 ];
 console.log("number of path: ", diagonalMaze(0, 0, board, ""));
