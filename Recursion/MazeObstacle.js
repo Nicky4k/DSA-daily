b00d661358861f35c7815910fff87f51daac485b
Nicky <nickysauceofficial@gmail.com>
2022-11-24 15:46:35 +0530

    maze with obstacle

--- Recursion/MazeObstacle.js
+++ Recursion/MazeObstacle.js
@@ -1,5 +1,6 @@
 // Diagonal, Down, Right Maze
 function diagonalMaze(a, b, path) {
+  if (a === obstacle[0] && b === obstacle[1]) return 0;
   if (a === 1 && b === 1) {
     console.log(path);
     return 1;
@@ -18,5 +19,5 @@ function diagonalMaze(a, b, path) {
   }
   return left + right + diag;
 }
-
+const obstacle = [2, 3];
 console.log("number of path: ", diagonalMaze(3, 3, ""));
