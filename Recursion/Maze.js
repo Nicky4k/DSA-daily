96156f455c492cc4ed65b2bf6d2f3daf708da9f1
Nicky <nickysauceofficial@gmail.com>
2022-11-24 15:14:46 +0530

    diagonal maze

--- Recursion/Maze.js
+++ Recursion/Maze.js
@@ -1,5 +1,6 @@
 console.log("Maze problem");
 
+// Down and Right movement maze
 function maze(a, b, path) {
   if (a === 1 && b === 1) {
     console.log(path);
@@ -14,3 +15,22 @@ function maze(a, b, path) {
 }
 
 maze(3, 3, "");
+
+// Diagonal, Down, Right Maze
+function diagonalMaze(a, b, path) {
+  if (a === 1 && b === 1) {
+    console.log(path);
+    return 1;
+  }
+  if (a > 1) {
+    diagonalMaze(a - 1, b, path + "Down ");
+  }
+  if (b > 1) {
+    diagonalMaze(a, b - 1, path + "Right ");
+  }
+  if (a > 1 && b > 1) {
+    diagonalMaze(a - 1, b - 1, path + "Diagonal ");
+  }
+}
+
+diagonalMaze(3, 3, "");
