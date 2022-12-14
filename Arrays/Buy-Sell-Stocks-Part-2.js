00388c7a2d49158186885f805b4c3521c51d39db
Nicky <nickysauceofficial@gmail.com>
2022-12-15 01:33:50 +0530

    Buy Sell Stocks multiple number of times

--- Arrays/Buy-Sell-Stocks-Part-2.js
+++ Arrays/Buy-Sell-Stocks-Part-2.js
@@ -2,8 +2,18 @@ console.log(
   "We can buy N number of times and sell N number of times, we must buy before Sell, we cannot short sell! What is the maximum profit we can make"
 );
 
-const tcsStocks = [19, 8, 2, 37, 4, 61, 9, 82, 37, 4, 6, 10, 8, 92, 7, 6];
+const teslaStockPrice = [5, 3, 9, 10, 1, 6, 4];
 
-function multiStocks(stocks) {}
+function multiStocks(stocks) {
+  let profit = 0;
+  let minPrice = stocks[0];
+  for (let i = 0; i < stocks.length; i++) {
+    if (stocks[i] > minPrice) {
+      profit += stocks[i] - minPrice;
+    }
+    minPrice = stocks[i];
+  }
+  return profit;
+}
 
-multiStocks(tcsStocks);
+multiStocks(teslaStockPrice);
